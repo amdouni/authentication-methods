595d98c39b358a9a0d19417018201a5dcbeae872
amdouni <amdouniraouf@gmail.com>
2025-03-06 15:55:39 +0100

    Update and rename init-symfony.sh to entrypoint.sh

--- .devcontainer/init-symfony.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ensure correct permissions
-chmod -R 777 var/cache var/log var/sessions
-
-# Start Symfony server
-symfony server:start --dir=/workspace --daemon
-
-echo "Symfony environment is ready!"
